431e48ec1429fce63510465850da198720ee5a5c
erikmronn <erikmronn@gmail.com>
2025-02-07 22:15:09 -0800

    add card hovering functionality
    Card manager receives signals from children cards when they're being hovered for emphasis

--- card_manager.gd
+++ card_manager.gd
@@ -4,6 +4,7 @@ const COLLISION_MASK_CARD = 1
 
 var screen_size
 var card_being_dragged
+var is_hovering_on_card
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -21,10 +22,45 @@ func _input(event):
 		if event.pressed:
 			var card = raycast_check_for_card()
 			if card:
-				card_being_dragged = card
+				start_drag(card)
 		else:
-			card_being_dragged = null
+			finish_drag()
 			
+func start_drag(card):
+	card_being_dragged = card
+	card.scale = Vector2(1, 1)
+	
+func finish_drag():
+	card_being_dragged.scale = Vector2(1.1, 1.1)
+	card_being_dragged = null
+	
+func connect_card_signals(card):
+	card.connect("hovered", on_hover_over_card)
+	card.connect("hovered_off", on_hover_off_card)
+	
+func on_hover_over_card(card):
+	if !is_hovering_on_card:
+		is_hovering_on_card = true
+		highlight_card(card, true)
+	
+func on_hover_off_card(card):
+	if !card_being_dragged:
+		highlight_card(card, false)
+		#Checks if we hover off a card straight onto another card
+		var new_card_hovered = raycast_check_for_card()
+		if new_card_hovered:
+			highlight_card(new_card_hovered, true)
+		else:
+			is_hovering_on_card = false
+	
+func highlight_card(card, hovered):
+	if hovered:
+		card.scale = Vector2(1.1, 1.1)
+		card.z_index = 2
+	else:
+		card.scale = Vector2(1, 1)
+		card.z_index = 1
+		
 # Checks if the mouse is hovering over a card, called in _process after checking
 # for LMB input. Returns card object if card otherwise returns null
 func raycast_check_for_card():
@@ -35,5 +71,21 @@ func raycast_check_for_card():
 	parameters.collision_mask = COLLISION_MASK_CARD
 	var result = space_state.intersect_point(parameters)
 	if result.size() > 0:
-		return (result[0].collider.get_parent())
+		#return (result[0].collider.get_parent())
+		return get_card_with_highest_z(result)
 	return null
+	
+	
+func get_card_with_highest_z(cards):
+	var highest_z_card = cards[0].collider.get_parent()
+	var highest_z_index = highest_z_card.z_index
+	
+	#look through all cards for highest z index one
+	for i in range (1, cards.size()):
+		var current_card = cards[i].collider.get_parent()
+		if current_card.z_index > highest_z_index:
+			highest_z_card = current_card
+			highest_z_index = current_card.z_index
+			
+	return highest_z_card
+	
