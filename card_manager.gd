25fc3d0f427b2d0c8359fb6d453d79ade7b4bb20
macly08 <maxwell.giltner@wsu.edu>
2025-02-21 15:31:15 -0800

    Changes card scaling to const var from hardcoded values

--- card_manager.gd
+++ card_manager.gd
@@ -3,6 +3,8 @@ extends Node2D
 const COLLISION_MASK_CARD = 1
 const COLLISION_MASK_CARD_SLOT = 2
 const DEFAULT_CARD_MOVE_SPEED = 0.1
+const DEFAULT_CARD_SCALE = 1.0
+const HIGHLIGHTED_CARD_SCALE = 1.1
 
 var screen_size
 var card_being_dragged
@@ -25,10 +27,10 @@ func _process(delta: float) -> void:
 			
 func start_drag(card):
 	card_being_dragged = card
-	card.scale = Vector2(1, 1)
+	card.scale = Vector2(DEFAULT_CARD_SCALE, DEFAULT_CARD_SCALE)
 	
 func finish_drag():
-	card_being_dragged.scale = Vector2(1.1, 1.1)
+	card_being_dragged.scale = Vector2(HIGHLIGHTED_CARD_SCALE, HIGHLIGHTED_CARD_SCALE)
 	var card_slot_found = raycast_check_for_card_slot()
 	if card_slot_found and not card_slot_found.card_in_slot:
 		player_hand_reference.remove_card_from_hand(card_being_dragged)
@@ -66,10 +68,10 @@ func on_hover_off_card(card):
 	
 func highlight_card(card, hovered):
 	if hovered:
-		card.scale = Vector2(1.1, 1.1)
+		card.scale = Vector2(HIGHLIGHTED_CARD_SCALE, HIGHLIGHTED_CARD_SCALE)
 		card.z_index = 2
 	else:
-		card.scale = Vector2(1, 1)
+		card.scale = Vector2(DEFAULT_CARD_SCALE, DEFAULT_CARD_SCALE)
 		card.z_index = 1
 		
 ## Checks if the mouse is hovering over a card, called in _process after checking
